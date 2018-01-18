d59272c7a1a7dc5c91fcc62ea2512886d1f01f3d
CORP\nkosinathi.khumalo <Nkosinathi.Khumalo@irdeto.com>
2018-01-18 09:59:42 +0200

    fix "fixed error on newman"

--- Run.bat
+++ Run.bat
@@ -1,2 +1,2 @@
 echo  "Run groovy script"
-"C:\Program Files (x86)\Jenkins\tools\hudson.plugins.groovy.GroovyInstallation\Groovy1.5.4\bin\groovy.bat" "Generate.groovy" & echo %USERNAME%
+"C:\Program Files (x86)\Jenkins\tools\hudson.plugins.groovy.GroovyInstallation\Groovy1.5.4\bin\groovy.bat" "Generate.groovy" & C:\Users\nkosinathi.khumalo\AppData\Roaming\npm\newman run "Collection1.json"
